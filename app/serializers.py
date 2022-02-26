050126c83ca73eba490fd3fdeb2b5a73ef32df06
gauravkhandelwal140 <gauravkhandelwal140@gmail.com>
2022-02-26 10:25:17 +0530

    'complete'

--- app/serializers.py
+++ app/serializers.py
@@ -6,6 +6,6 @@ from rest_framework import filters
 class User_serializers(serializers.ModelSerializer):
     class Meta:
         model=User
-        fields=['first_name','last_name','company_name','age','city','state','zip','web','email',]
+        fields=['id','first_name','last_name','company_name','age','city','state','zip','web','email',]
 
 
